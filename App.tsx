72983587e3431458d084c8f9508fb31959acdf59
Bruno Frohlich <68931062+brunopp00@users.noreply.github.com>
2023-01-09 15:01:53 -0300

    TESTE

--- App.tsx
+++ App.tsx
@@ -1,7 +1,12 @@
 import { Groups } from './src/screens/Groups';
+import { ThemeProvider } from 'styled-components'
+
+import theme from './src/themes'
 
 export default function App() {
   return (
-    <Groups/>
+    <ThemeProvider theme={theme}>
+      <Groups/>
+    </ThemeProvider>
   );
 }
