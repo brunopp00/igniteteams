02e67ae7e0da4909d1a7a9967e060bce3631ad19
brunopp00 <brunofnh08@gmail.com>
2023-01-15 16:34:28 -0300

    f

--- App.tsx
+++ App.tsx
@@ -1,12 +1,20 @@
 import { Groups } from './src/screens/Groups';
 import { ThemeProvider } from 'styled-components'
-
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'
+import { Loading } from '@components/Loading';
 import theme from './src/themes'
 
 export default function App() {
+
+  const [fonts] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+  });
+
+
   return (
     <ThemeProvider theme={theme}>
-      <Groups/>
+      { fonts ? <Groups/> : <Loading/>}
     </ThemeProvider>
   );
 }
